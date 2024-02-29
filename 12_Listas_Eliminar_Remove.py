f3898515c4b14b79e445f345c2c4870f55894c29
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 22:27:25 -0600

    Eliminación de elementos con el remove en lista

--- 12_Listas_Eliminar_Remove.py
+++ 12_Listas_Eliminar_Remove.py
@@ -4,3 +4,4 @@
 automoviles = ["Honda", "Fiat", "BMW", "Ford", "Kia"]#se crea una lista con 5 palabras diferentes 
 automoviles.remove("Ford")#se utiliza el del para elimiar un caracter de la lista dandole el nombre del caracter
 print(automoviles) #se imprime lo que esta dentro de la lista
+
