de02946a965ce897135ce60f6e18fe9b0afdb5a0
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 23:56:31 -0600

    Utilizando el Bucle While

--- 26_Bucle_While.py
+++ 26_Bucle_While.py
@@ -1,17 +1,27 @@
 
 #ejemplo de un bucle infinito
 
-x = 1 #se declara una variable y se iguala a un entero
+num = 5 #se declara una variable y se iguala a un entero
 
-while x < 10: #se crea un bucle infinito hasta que por un medio externo o manual detenga la ejecucion 
-	print(x)#se imprime la variable
-	x += 1 #se añade un incremento para que cambie su valor numerico
+while num < 10: #se crea un bucle infinito hasta que por un medio externo o manual detenga la ejecucion 
+	print(num)#se imprime la variable
+	num += 1 #se añade un incremento para que cambie su valor numerico
 
 #se usa el += para imcremento y -= para el decramiento
 print("\n")
 
-y = 9 #se declara una variable y se igauala a un entero
+num2 = 12 #se declara una variable y se igauala a un entero
 
-while y > 0: #se crea un bucle infinito hasta que por un medio externo o manual detenga la ejecucion 
-	print(y)#se imprime la variable
-	y -= 1 #se añade un incremento para que cambie su valor numerico
+while num2 > 0: #se crea un bucle infinito hasta que por un medio externo o manual detenga la ejecucion 
+	print(num2)#se imprime la variable
+	num2 -= 1 #se añade un incremento para que cambie su valor numerico
+#Ejemplo
+print("\n")
+
+#Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5.
+
+x = 0
+while x <= 15:
+        print(x)
+        x += 5
+        
