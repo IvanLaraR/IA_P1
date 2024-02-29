a01ce4ed959720466ad6177103e38fcf644f5c05
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 23:04:31 -0600

    Como convertir tuplas a listas y viceversa

--- 19_Convertir_Tuplas_Listas.py
+++ 19_Convertir_Tuplas_Listas.py
@@ -5,6 +5,7 @@ tupla = tuple(lista)#se crea una variable tupla donde se usa el metodo tuple que
 print(tupla)#se imprime la nueva tupla
 print(type(tupla)) #se utiliza el metodo type para saber que clase es la variable
 
+print("\n")
 #Convertir una tupla a una lista
 tupla = ("Honda", "Fiat", "BMW", "Ford", "Kia", "Bentley", "Tesla", "Toyota")#se crea una tupla con 8 elementos diferentes
 lista = list(tupla)#se crea una variable lista donde se usa el metodo list para convertir la tupla a una lista 
