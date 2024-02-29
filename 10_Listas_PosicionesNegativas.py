573115f6f3ae038a0183d36d1e9a9275de7b0c6d
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 22:18:50 -0600

    Posiciones Negativas de una Lista

--- 10_Listas_PosicionesNegativas.py
+++ 10_Listas_PosicionesNegativas.py
@@ -1,4 +1,5 @@
 #se reutilizo el codigo pasado
+
 automoviles = ["Honda", "Fiat", "BMW", "Ford", "Kia"]#se crea una lista con 5 palabras diferentes 
 print(automoviles[-1])#se usa el print para imprimir la lista y hora queremos llamar un numero negativo entonces va del ultimo al primero o derecha a izquierda
 
