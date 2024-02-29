1522fcaf93dce58dcc6fb93da442b868c715b776
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 22:26:33 -0600

    Eliminación de elementos con el del en lista

--- 10_Listas_PosicionesNegativas.py
+++ 10_Listas_PosicionesNegativas.py
@@ -3,3 +3,9 @@
 automoviles = ["Honda", "Fiat", "BMW", "Ford", "Kia"]#se crea una lista con 5 palabras diferentes 
 print(automoviles[-1])#se usa el print para imprimir la lista y hora queremos llamar un numero negativo entonces va del ultimo al primero o derecha a izquierda
 
+print("\n")
+
+#Utiliza las posiciones negativas para acceder e imprimir algunos de los colores de esta lista. Los colores a los que tienes que acceder son 'naranja', 'amarillo', 'lila', 'blanco' y 'rojo'.
+
+colores = ['naranja', 'amarillo', 'lila', 'blanco' , 'rojo']
+print(colores[-5])
--- 11_Listas_Eliminar_Del.py
+++ 11_Listas_Eliminar_Del.py
@@ -9,3 +9,4 @@ print("\n")#se pone un salto de linea para que se vea mas ordenado
 automoviles = ["Honda", "Fiat", "BMW", "Ford", "Kia"]#se crea una lista con 5 palabras diferentes 
 del automoviles[-1]#se utiliza el del para elimiar un caracter de la lista dandole la pocision en la que esta colocado
 print(automoviles)#se imprime le que esta en la lista
+
