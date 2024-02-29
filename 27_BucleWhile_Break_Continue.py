3e48c63e915e6ea439acf44e0f421bbb9b133188
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 23:58:21 -0600

    Utilizando el Bucle While con Condicional If

--- 27_BucleWhile_Break_Continue.py
+++ 27_BucleWhile_Break_Continue.py
@@ -12,11 +12,11 @@ while y <= 20: #se crea un bucle x menor o igual a 20
 #la funcion del continue es para hacer saltos dentro del bucle while
 print("\n")
 
-x = 0 #se declara una variable
+z = 0 #se declara una variable
 
-while x < 10: #se crea un bucle x menor que 10
-	x += 1 #se va incrementendo el valor 1 por 1 
-	if x == 2 or x == 9: #se crea un condicional si x es igual a 5 o 7 se cumple 
+while z < 20: #se crea un bucle x menor que 10
+	z += 1 #se va incrementendo el valor 1 por 1 
+	if z == 2 or z == 9: #se crea un condicional si x es igual a 5 o 7 se cumple 
 		continue #se usa el metodo continue para hacer el salto de los valores 5 y 7 
-	print(x) #se imprime la varible x 
+	print(z) #se imprime la varible x 
 
