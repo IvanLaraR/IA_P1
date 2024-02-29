d77dc9e83185e89b27bd9ae286261a73c202fef4
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 23:24:44 -0600

    Usando el condicional if con el else

--- 21_Condicional_If_Else.py
+++ 21_Condicional_If_Else.py
@@ -5,3 +5,15 @@ if edad >= 18:#se hace una condicion si la edad es mayor a 18 sea un true
 else: #cuando la condicion sea false se ejecutara esta parte de codigo
     print("Perdon No puedes entrar")#se imprime el mensaje
 
+#EJERCICIOS
+print("\n")
+
+#Corrige el siguiente condicional if else
+
+color = "rojo"
+
+if color == "rojo":
+    print("El color no es rojo.")
+else:
+    print ("El color es rojo.")
+
