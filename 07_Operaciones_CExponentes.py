f1e9de7237c9672c5bb66154752d977a341bb800
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 22:06:51 -0600

    Uso de Exponentes

--- 07_Operaciones_CExponentes.py
+++ 07_Operaciones_CExponentes.py
@@ -1,10 +1,15 @@
-#se usa el ** usalo como exponentes ejem:10 * 10
-elevado = 10 ** 2 #se declara una variable y se le asigna un valor numerico que eleva al cuadrado
-print(elevado) #se usa el print para imprimir y se llama a la varible
+#se usa el ** usalo como exponentes ejem:2 * 2
+num = 2 ** 2 #se declara una variable y se le asigna un valor numerico que eleva al cuadrado
+print(num) #se usa el print para imprimir y se llama a la varible
 
 print("\n")#se hace un salto de linea para que se vea mas ordenado 
 
-#ejem 5**5 = 5 * 5 = 25 * 5 = 125 * 5 = 625 * 5 = 312
-elevado = 5 ** 5 #se declara una variable y se le asigan un valor numerico que esta a la quinta potencia
-print(elevado)# se usa el print para imprimir y se llama a la varibale
+#ejem 2**6 = 2 * 2 = 4 * 2 = 8 * 2 = 16 * 2 = 32 * 2 =64
+num = 2 ** 6 #se declara una variable y se le asigan un valor numerico que esta a la quinta potencia
+print(num)# se usa el print para imprimir y se llama a la varibale
 
+print("\n")
+
+#Utilizando solo el número 2 (cuantas veces necesites) y sin utilizar exponentes, debes obtener el resultado 1024
+resultado = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+print(resultado)
