7e743a4c6ba501a341e5042338d2bd219395c7a2
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 23:44:59 -0600

    Utilizando Multiples condiciones If sin elif

--- 24_MultiplesC_If.py
+++ 24_MultiplesC_If.py
@@ -1,13 +1,15 @@
 #como encadenar multiples condiciones sin elif o else
 
-entrada = int(input("Introduce un numero de 1 al 3:\t")) #se crea una variabel entrada y se igauala a un entero qeu proporciona el usuario
+print("\t\tElije un numero del 1 al 3 ")
+print("\n")
+obj = int(input("Introduce un numero:\t")) #se crea una variabel entrada y se igauala a un entero qeu proporciona el usuario
 
-if entrada == 1:#se usa una condicion si el numero que eligio el usuario es igual a 1 es cumple y se ejecuta
-    print("haz seleccionado el numero 1")#se imprime el mensaje
+if obj == 1:#se usa una condicion si el numero que eligio el usuario es igual a 1 es cumple y se ejecuta
+    print("Felicidades has ganado un auto")#se imprime el mensaje
 
-if entrada == 2:#se usa una condicion si el numero que eligio el usuario es igual a 2 es cumple y se ejecuta
-    print("haz seleccionado el numero 2")#se imprime el mensaje
+if obj == 2:#se usa una condicion si el numero que eligio el usuario es igual a 2 es cumple y se ejecuta
+    print("Sorry,sigue participando")#se imprime el mensaje
 
-if entrada == 3:#se usa una condicion si el numero que eligio el usuario es igual a 3 es cumple y se ejecuta
-    print("haz seleccionado el numero 3")#se imprime el mensaje
+if obj == 3:#se usa una condicion si el numero que eligio el usuario es igual a 3 es cumple y se ejecuta
+    print("Felicidades has ganado un mango")#se imprime el mensaje
     
