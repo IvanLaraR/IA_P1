6ba11c586f6f672921a4f72b2482db167627182a
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-29 00:10:28 -0600

    Cómo crear y llamar Funciones

---
+++ 33_CrearLLamar_Funciones.py
@@ -0,0 +1,12 @@
+#para creaer funciones se utiliza la palabra reservada DEf (la llamas cuando la quieras usar)
+
+def Grita(): #definimos la palabra reservada def y le llamamos Grita
+    print("ahhhhhhhhh")#se imprime el mensaje 
+Grita()#se llama a la palabra reservada y se ejecuta
+
+print("\n")
+
+def Grita(Motivo, emoji): #definimos la palabra reservada def y le llamamos Grita
+    print("ahhhhhhhhh", Motivo, emoji)#se imprime el mensaje
+Grita("Mis hijossss", ":0")
+Grita("aamen", ";p")#se llama a la palabra reservada y se ejecuta
