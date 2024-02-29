bc96d8231e1b71556deb5bfa605ad09734e890d8
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 22:35:47 -0600

    Insertar de elementos con el append en listas

--- 14_Listas_Insertar_Append.py
+++ 14_Listas_Insertar_Append.py
@@ -1,5 +1,18 @@
 
 #Agregar un nuevo carater a la lista
+
 automoviles = ["Honda", "Fiat", "BMW", "Ford", "Kia", "Bentley", "Tesla", "Toyota"]#se crea una lista con 5 palabras diferentes 
 automoviles.append("Mazda")#se utiliza la variable y se le agrega el metodo append para agregar un caracter por el nombre de la lista 
 print(automoviles) #se imprime lo que esta dentro de la lista
+
+print("\n")
+
+#EJERCICIO
+#Añade los colores 'fuxia' y 'celeste' a la lista.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.append("Fuxia")
+colores.append("celeste")
+print(colores)
+
+
