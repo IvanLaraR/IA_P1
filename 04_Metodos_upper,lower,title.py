3728285c91e0565842576d8bf3a9f7ba2db46892
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 21:45:02 -0600

    Uso de Metodos (upper,lower,title)

--- 04_Metodos_upper,lower,title.py
+++ 04_Metodos_upper,lower,title.py
@@ -1,17 +1,18 @@
 #El método title(poner la primera palabra en mayusculas)
-nombre = "curso python" #se declara una variable que va a ser el primer mensaje en un string
-nombre = nombre.title() #se le cambia el valor a la varibale poniendo en segida el metodo .title()
-print(nombre)#se usa el print para imprimir y mostrarlo en la pantalla
+
+var = "curso python" #se declara una variable que va a ser el primer mensaje en un string
+var = var.title() #se le cambia el valor a la varibale poniendo en segida el metodo .title()
+print(var)#se usa el print para imprimir y mostrarlo en la pantalla
 
 #////Abreviando el codigo////
 
-nombre = "curso python".title()#se declara la variable con su mensaje string y se le agrega el metodo .title()
-print(nombre)#se usa el print para imprimir y mostarlo en la pantalla
+var = "curso python".title()#se declara la variable con su mensaje string y se le agrega el metodo .title()
+print(var)#se usa el print para imprimir y mostarlo en la pantalla
 
 #El método upper(todos los caracteres de un string se hacen mayusculas)
-nombre = "curso python".upper()#se declara la variable con su mensaje string y se le agrega el metodo .upper()
-print(nombre)#se usa el print para imprimir y mostrarlo en la pantalla
+var = "curso python".upper()#se declara la variable con su mensaje string y se le agrega el metodo .upper()
+print(var)#se usa el print para imprimir y mostrarlo en la pantalla
 
 #El metodo lower (todos los caracteres de un string se hacen minusculas)
-nombre = "CURSO pYtHoN".lower()#se declara una variable con su mensaje string y se le agrega el metodo .lower()
-print(nombre)#se usa el print para imprimir y mostrarlo en la pantalla
+var = "CURSO pYtHoN".lower()#se declara una variable con su mensaje string y se le agrega el metodo .lower()
+print(var)#se usa el print para imprimir y mostrarlo en la pantalla
