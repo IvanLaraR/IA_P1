bd308879bc4718e4e406ab4fd84ec4e5c9df66f2
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 22:12:23 -0600

    Uso de decimales utilizando Float

--- 08_DatoFloat_Round.py
+++ 08_DatoFloat_Round.py
@@ -1,14 +1,19 @@
 
-num1 = 10.45 #se crea una variable y se le asigna un valor numerico con 2 numeros despues del punto
-num2 = 45.9768 #se crea variable y se le asigna un valor numerico con 4 numeros despues del punto
-print(num1+num2) #se usa el print para imprimir y se llamn los dos varibles y se suman
+var = 87.82 #se crea una variable y se le asigna un valor numerico con 2 numeros despues del punto
+var2 = 21.98876 #se crea variable y se le asigna un valor numerico con 4 numeros despues del punto
+print(var+var2) #se usa el print para imprimir y se llamn los dos varibles y se suman
 
 print("\n")#se hace un salto de linea para que se vea mas ordenado
 
 
-num1 = 10.99 #se crea una variable y se le asigna un valor numerico con 2 numeros despues del punto
+num1 = 65.99 #se crea una variable y se le asigna un valor numerico con 2 numeros despues del punto
 num2 = 25.9228 #se crea una variable y se le asigna un valor numerico con 4 numeros despues del punto
-resul = num1+num2 #se crea una variable que se iguala a la suma de las dos primeras variables 
+resul = var+var2 #se crea una variable que se iguala a la suma de las dos primeras variables 
 print(round(resul,2)) #se imprime la varible resul pero se hace utilizando el metodo round(var,y el numero de decimales que quieres al final)
 
+print("\n")
+#Redondea con cinco decimales el número 17.567383292929200234
+
+var = 17.567383292929200234
+print(round(var,5))
 
