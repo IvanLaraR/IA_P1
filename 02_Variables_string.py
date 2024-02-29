7456733af5331424e9a29838776c2c6328d35927
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 21:30:31 -0600

    Uso de una variable con un string

--- 02_Variables_string.py
+++ 02_Variables_string.py
@@ -1,5 +1,6 @@
-mensaje = "Esto es un 'String'"#se declara la varible mensaje y se la asigna el cometario utlizando "" y adentro del mismo su esa '' como función string
-print(mensaje)#se usa el print para imprimir el mensaje y mostarlo en la pantalla
+var = "Este es un mensaje usando comillas simples utilizando un 'String'"#se declara la varible  y se la asigna el cometario utlizando "" y adentro del mismo su esa '' como función string
+print(var)#se usa el print para imprimir el mensaje y mostarlo en la pantalla
 
-mensaje = 'Esto es un "String"'#se declara la varible mensaje y se la asigna el cometario utlizando '' y adentro del mismo su esa "" como función string
-print(mensaje)#se usa el print para imprimir el mensaje y mostarlo en la pantalla
+var = 'Este es un mensaje usando comillas dobles utilizando un  "String"'#se declara la varible y se la asigna el cometario utlizando '' y adentro del mismo su esa "" como función string
+print(var)#se usa el print para imprimir el mensaje y mostarlo en la pantalla
+
