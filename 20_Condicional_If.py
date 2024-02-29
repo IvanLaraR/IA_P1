c0cf8e7f512e7012a94373d8a88616b6235be9fa
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 23:13:04 -0600

    Uso del condicional if

--- 20_Condicional_If.py
+++ 20_Condicional_If.py
@@ -1,18 +1,24 @@
 #si el if es false no se ejecuta esa parte del codigo y lo pasa
-num1 = 10#se declara una variable
-num2 = 20#se declara una variable
 
-if num1 == num2: #se utiliza el condicional if para decir si es igual las dos variables se ejecute 
-    print("se ejecuta")#si la condicion es true se ejecutara y mostrara el mensaje que se imrpime
+var = 50#se declara una variable
+var2 = 30#se declara una variable
 
+if var == var2: #se utiliza el condicional if para decir si es igual las dos variables se ejecute 
+    print("se cumple")#si la condicion es true se ejecutara y mostrara el mensaje que se imrpime
 
-num1 = 10#se declara una variable
-num2 = 20#se declara una variable
+print("\n")
+var = 2#se declara una variable
+var2 = 6#se declara una variable
 
-if num1 < num2: #se utiliza el condicional if para decir si es menor que las dos variables se ejecute 
-    print("se ejecuta")#si la condicion es true se ejecutara y mostrara el mensaje que se imrpime
+if var < var2: #se utiliza el condicional if para decir si es menor que las dos variables se ejecute 
+    print("se cumple")#si la condicion es true se ejecutara y mostrara el mensaje que se imrpime
 
+#Ejercicos
 
+var = 15
+var2 = 20
 
+if var != var2:
+	print('Se ejecuta el if.')
 
 
