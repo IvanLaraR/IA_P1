923464df43e918b2a052acea4e3029ecefc3650b
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-28 21:21:08 -0600

    Uso de Variables en python

--- 01_Variables_int.py
+++ 01_Variables_int.py
@@ -1,14 +1,14 @@
 
-mensaje = "Bienvenidos al Cuso" #se imprime el mensaje con la variable "menaje"
-print(mensaje) #se escribe la funcion print para imprimir lo que contenga la variable
+variable = "Bienvenidos al Cuso" #se imprime el mensaje con la variable "menaje"
+print(variable) #se escribe la funcion print para imprimir lo que contenga la variable
 
-mensaje = "Bienveindos al curso" #se imprime el mensaje con la variable "menaje"
-mensaje = 10 #se le cambia de valor a nuestra variable ya declarada 
-print(mensaje)#se escribe la funcion print para imprimir lo que contenga la variable
+variable = "Bienveindos al curso" #se imprime el mensaje con la variable "menaje"
+variable = 500 #se le cambia de valor a nuestra variable ya declarada 
+print(variable)#se escribe la funcion print para imprimir lo que contenga la variable
 
-mensaje = "Bienveindos al curso" #se imprime el mensaje con la variable "menaje"
-mensaje = 10 #se le cambia de valor a nuestra variable ya declarada
-print(mensaje + 20) #se imprime el valor del mensaje que vale 10 pero se la suma el numero 20
+variable = "Bienveindos al curso" #se imprime el mensaje con la variable "menaje"
+variable = 500 #se le cambia de valor a nuestra variable ya declarada
+print(variable - 50) #se imprime el valor del mensaje que vale 10 pero se la suma el numero 20
 
 
 
