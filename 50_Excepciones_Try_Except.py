dcfbbd09536db2fc90cb16cc636b151ca266709c
IvanLaraR <ivanlaruv102@gmail.com>
2024-02-29 20:42:00 -0600

    Manejo de Excepciones try y except

---
+++ 50_Excepciones_Try_Except.py
@@ -0,0 +1,19 @@
+#El bloque except: permite manejar errores en caso de que estos ocurran.
+
+
+try: # se utiliza la excepcion, se ejecuta el bloque de prueba
+    print(var) #se imprime la variable
+
+except: #se salta la ejecucion en caso de que curra
+    print("No hay nada ")
+
+print("\n")
+
+#El bloque try:permite probar un bloque de código en busca de errores.
+
+var = "Afirmativo"
+try: # se utiliza la excepcion, se ejecuta el bloque de prueba
+    print(var) #se imprime la variable
+
+except: #se salta la ejecucion en caso de que curra
+    print("No hay nada ")
